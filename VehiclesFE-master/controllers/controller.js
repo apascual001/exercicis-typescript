7f20ce40152257be377e866ce9035f03b1a5019c
Ana Pascual <annapascual001@gmail.com>
2019-12-17 12:00:12 +0100

    Fase cotxe amb matrícula i marca

--- VehiclesFE-master/controllers/controller.js
+++ VehiclesFE-master/controllers/controller.js
@@ -1,23 +1,61 @@
 "use strict";
-//        let form = <HTMLElement>document.getElementById('loginForm');
-//          form.addEventListener('submit', (event) => {event.preventDefault(); this.submitForm()});
-var CarPanel = /** @class */ (function () {
-    function CarPanel() {
-        var _this = this;
-        var form = document.getElementById('vform');
-        form.addEventListener('submit', function (event) { event.preventDefault(); _this.submitForm(); });
-    }
-    CarPanel.prototype.submitForm = function () {
-    };
-    return CarPanel;
-}());
-new CarPanel();
-function createCar(plate, brand, color) {
+//let form = <HTMLElement>document.getElementById('vform');
+//      form.addEventListener('submit', (event) => {event.preventDefault(); submitCar()});
+//document.getElementById("#enviar").addEventListener("click", function(event) {
+// event.preventDefault();
+//}, false);
+var car;
+var plate_error = document.getElementById('plate_error');
+var color_error = document.getElementById('color_error');
+var brand_error = document.getElementById('brand_error');
+function comprobar() {
+    //event.preventDefault()
     var plate = document.getElementById("plate").value;
     var color = document.getElementById("color").value;
     var brand = document.getElementById("brand").value;
-    var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
+    car = new Car(plate, color, brand);
+    var x = 0;
+    var expreg = /^[a-zA-Z]{4}[\d]{3}$/;
+    if (expreg.test(plate)) {
+        plate_error.innerHTML = '';
+        console.log('true');
+    }
+    else {
+        plate_error.innerHTML = 'La matrícula debe tener 4 letras y 3 números';
+        x++;
+        console.log(plate);
+        console.log(false);
+        console.log(x);
+    }
+    if (color == "") {
+        color_error.innerHTML = 'El color es requerido';
+        x++;
+        console.log(x);
+    }
+    if (brand == "") {
+        brand_error.innerHTML = 'La marca es requerido';
+        x++;
+        console.log(x);
+    }
+    return x;
+}
+// Limpiar
+function limpiar() {
+    var plate = document.getElementById("plate").value;
+    var color = document.getElementById("color").value;
+    var brand = document.getElementById("brand").value;
+    var expreg = new RegExp("^[a-zA-Z]{4}[\d]{3}$ ");
+    if (expreg.test(plate)) {
+        plate_error.innerHTML = '';
+    }
+    if (color != "") {
+        color_error.innerHTML = "";
+    }
+    if (brand != "") {
+        brand_error.innerHTML = "";
+    }
+}
+function showCar() {
     var showInfo = document.getElementById('showInfo');
     var elementPlate = document.createElement('div');
     var elementColor = document.createElement('div');
@@ -28,4 +66,21 @@ function createCar(plate, brand, color) {
     showInfo.appendChild(elementColor);
     elementBrand.innerHTML = 'MARCA COCHE :  ' + car.brand;
     showInfo.appendChild(elementBrand);
+}
+function submitCar() {
+    event.preventDefault();
+    limpiar();
+    var x = comprobar();
+    if (x == 0) {
+        console.log(x);
+        showCar();
+        var button = document.getElementById("enviar");
+        button.classList.add('hide');
+        var wheelsInfo = document.getElementById("wheelsInfo");
+        wheelsInfo.classList.remove('hide');
+        console.log(car);
+    }
+    else {
+        return false;
+    }
 }
--- VehiclesFE-master/controllers/controller.ts
+++ VehiclesFE-master/controllers/controller.ts
@@ -1,27 +1,67 @@
-//        let form = <HTMLElement>document.getElementById('loginForm');
-//          form.addEventListener('submit', (event) => {event.preventDefault(); this.submitForm()});
+//let form = <HTMLElement>document.getElementById('vform');
+  //      form.addEventListener('submit', (event) => {event.preventDefault(); submitCar()});
+//document.getElementById("#enviar").addEventListener("click", function(event) {
+           // event.preventDefault();
+   //}, false);
 
-class CarPanel {
+let car: Car;
 
-    constructor() {
-        const form = <HTMLElement>document.getElementById('vform');
-        form.addEventListener('submit', (event) => {event.preventDefault(); this.submitForm()});
-    }
-    public submitForm(): void {
-        }   
-}
+var plate_error = <HTMLElement>document.getElementById('plate_error');
+const color_error = <HTMLElement>document.getElementById('color_error');
+const brand_error = <HTMLElement>document.getElementById('brand_error');
 
-new CarPanel();
-
-function createCar(plate:string,brand:string,color:string){
-    
+function comprobar(){
+    //event.preventDefault()
     var plate = (<HTMLInputElement>document.getElementById("plate")).value;
     var color = (<HTMLInputElement>document.getElementById("color")).value;
     var brand = (<HTMLInputElement>document.getElementById("brand")).value;
- 
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));   
 
+    car=new Car(plate,color,brand);
+
+    var x = 0; 
+    var expreg = /^[a-zA-Z]{4}[\d]{3}$/;
+    if (expreg.test(plate)) {
+        plate_error.innerHTML = '';
+        console.log('true');
+    }
+    else{
+        plate_error.innerHTML = 'La matrícula debe tener 4 letras y 3 números';
+        x ++;
+        console.log(plate);
+        console.log(false);
+        console.log(x);
+    }
+    if (color == "") {
+        color_error.innerHTML = 'El color es requerido';
+        x ++;
+        console.log(x);
+    }
+    if (brand == "") {
+        brand_error.innerHTML = 'La marca es requerido';
+        x ++;
+        console.log(x);
+    }
+    return x;
+}
+// Limpiar
+function limpiar(){
+    var plate = (<HTMLInputElement>document.getElementById("plate")).value;
+    var color = (<HTMLInputElement>document.getElementById("color")).value;
+    var brand = (<HTMLInputElement>document.getElementById("brand")).value;
+
+    var expreg = new RegExp("^[a-zA-Z]{4}[\d]{3}$ ");
+    if (expreg.test(plate)) {
+        plate_error.innerHTML = '';
+    }
+    if (color != "") {
+      color_error.innerHTML = "";
+    }
+    if (brand != "") {
+        brand_error.innerHTML = "";
+    }  
+  }
+  
+function showCar(){
     const showInfo = <HTMLElement>document.getElementById('showInfo');
     const elementPlate: HTMLElement = document.createElement('div');
     const elementColor: HTMLElement = document.createElement('div');
@@ -33,5 +73,22 @@ function createCar(plate:string,brand:string,color:string){
     showInfo.appendChild(elementColor);
     elementBrand.innerHTML = 'MARCA COCHE :  ' + car.brand;
     showInfo.appendChild(elementBrand);
+}
 
+function submitCar() {
+    event.preventDefault()
+    limpiar();
+    var x = comprobar();
+    if (x == 0) {
+        console.log(x);
+        showCar();
+        var button = <HTMLElement>document.getElementById("enviar");
+        button.classList.add('hide');
+        var wheelsInfo = <HTMLElement>document.getElementById("wheelsInfo");
+        wheelsInfo.classList.remove('hide');
+        console.log(car);
+
+    } else {
+    return false;
+    } 
 }
